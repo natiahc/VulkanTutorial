88c13c2a9556fbf5beb718cbd9cef21c0dbc6387
Chaitan Divagi <natiahc@gmail.com>
2019-03-29 20:17:47 +0530

    understands VkInstanceCreateInfo;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

--- VulkanTutorial/VulkanTutorial.cpp
+++ VulkanTutorial/VulkanTutorial.cpp
@@ -15,6 +15,17 @@ int main()
 	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 	appInfo.apiVersion = VK_API_VERSION_1_0;
 
+	VkInstanceCreateInfo instanceInfo;
+
+	instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceInfo.pNext = NULL;
+	instanceInfo.flags = 0;
+	instanceInfo.pApplicationInfo = &appInfo;
+	instanceInfo.enabledLayerCount = 0;
+	instanceInfo.ppEnabledLayerNames = NULL;
+	instanceInfo.enabledExtensionCount = 0;
+	instanceInfo.ppEnabledExtensionNames = NULL;
+
     return 0;
 }
 
