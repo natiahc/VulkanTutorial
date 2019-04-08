879e12c046dcce967d9e1d44bd73fb629f212b6e
Chaitan Divagi <natiahc@gmail.com>
2019-04-08 23:17:07 +0530

    VkPipelineViewportStateCreateInfo;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

--- VulkanTutorial/VulkanTutorial.cpp
+++ VulkanTutorial/VulkanTutorial.cpp
@@ -416,6 +416,15 @@ void startVulkan()
 	scissor.offset = { 0, 0 };
 	scissor.extent = { WIDTH, HEIGHT };
 
+	VkPipelineViewportStateCreateInfo viewportStateCreateInfo;
+	viewportStateCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+	viewportStateCreateInfo.pNext = nullptr;
+	viewportStateCreateInfo.flags = 0;
+	viewportStateCreateInfo.viewportCount = 1;
+	viewportStateCreateInfo.pViewports = &viewport;
+	viewportStateCreateInfo.scissorCount = 1;
+	viewportStateCreateInfo.pScissors = &scissor;
+
 	delete[] swapchainImages;
 	delete[] layers;
 	delete[] extensions;
